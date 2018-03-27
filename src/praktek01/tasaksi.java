8e9f8800c9b7c7d771c734cddadbad98464fd07d
dinanuroktavia <nuroktaviadina@gmail.com>
2018-03-27 22:34:58 +1100

    mengisi atribut

--- src/praktek01/tasaksi.java
+++ src/praktek01/tasaksi.java
@@ -12,6 +12,30 @@ package praktek01;
 public class tasaksi {
     public static void main(String[] args) {
         
+        tas A = new tas();
+        tas B = new tas();
+        tas C = new tas();
+        
+        
+        A.bahan="kulit";
+        A.merk="reebok";
+        A.warna="hitam" ;       
+        A.harga="400.000";
+        A.jenis="tas olahraga";       
+        
+        B.bahan="kain";
+        B.merk="canvas";
+        B.warna="hitam" ;       
+        B.harga="300.000";
+        B.jenis="tas sekolah";
+        
+        C.bahan="levis";
+        C.merk="convers";
+        C.warna="abu abu" ;       
+        C.harga="500.000";
+        C.jenis="tas santai";
+        
+        
     }
     
 }
